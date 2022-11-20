9c2cdb696091b3e1163f3fcc225d6bf05729a703
JohnathanBaldera <74841308+JohnathanBaldera@users.noreply.github.com>
2022-11-19 22:25:36 -0600

    Food data for California uploaded

--- components/HomeMap.js
+++ components/HomeMap.js
@@ -1,19 +1,29 @@
 import Map from './map';
 import styles from '../styles/Home.module.css';
+import foodBankData from '../data-20221020221251.json'
 
 
 export const HomeMap = () => {
   return (
-    <Map className={styles.homeMap} center={[38.907132, -77.036546]} zoom={12}>
+    <Map className={styles.homeMap} center={[34.0522, -118.2437]} zoom={12}>
       {({ TileLayer, Marker, Popup }) => (
         <>
           <TileLayer
             url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           />
-          <Marker position={[38.907132, -77.036546]}>
+          {foodBankData.data.map(foodbank => {
+            return (
+              <Marker position={[foodbank[19], foodbank[20]]} key={foodbank[1]}>
+                <Popup>
+                  {foodbank[8]}
+                </Popup>
+              </Marker>
+            )
+          })}
+          <Marker position={[34.0522, -118.2437]}>
             <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
+              Example Food Shelter Location
             </Popup>
           </Marker>
         </>
