43d3fc3ae0b43b40eb31bafd418f5bfe7d7c33ed
trzaidi <trzaidi@gmail.com>
2022-11-20 15:32:02 -0500

    search tab/button

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,6 +2,7 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import HomeIcon from '@mui/icons-material/Home';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import MonitorHeart from '@mui/icons-material/MonitorHeart';
+import LocationSearching from '@mui/icons-material/LocationSearching';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import { useState } from 'react';
 
@@ -18,6 +19,7 @@ export const Nav = () => {
       <BottomNavigationAction label='Home' icon={<HomeIcon />} />
       <BottomNavigationAction label='Favorites' icon={<FavoriteIcon />} />
       <BottomNavigationAction label='Diabetic' icon={<MonitorHeart />} />
+      <BottomNavigationAction label='Search' icon={<LocationSearching />} />
     </BottomNavigation>
   );
 };
